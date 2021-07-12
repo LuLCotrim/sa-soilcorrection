d8d8ff9cc37f82abe2884ab29ad14b4c813806f4
LuLCotrim <lucaslcotrim@hotmail.com>
2021-07-11 23:41:22 -0300

    Criado métodos de teste para cálculo de participação atual, ideal e corrigida do cálcio

--- src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TestaCorrecaoCalcioMagnesio.java
+++ src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TestaCorrecaoCalcioMagnesio.java
@@ -2,6 +2,8 @@ package edu.utfpr.cp.dacom.sa.soilcorrection;
 
 import org.junit.jupiter.api.Test;
 
+import jdk.jfr.Timestamp;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestaCorrecaoCalcioMagnesio{
@@ -18,6 +20,24 @@ public class TestaCorrecaoCalcioMagnesio{
     @Test
     public void calcIdealTest(){
         CorERecCaMg c = new CorERecCaMg();
-        
+        c.setTexSolo(1);
+        assertEquals("45 a 55", c.calcIdeal());
+    }
+
+    @Test
+    public void calcPartCorrigidoCalcioTest(){
+        CorERecCaMg c = new CorERecCaMg();
+        c.setFonteP(1);
+        c.setTeorP(12);
+        c.setQtdP(8.59);
+        c.setEfcP(70);
+        c.setFonteCorretivo(2);
+        c.setQtdCa(5.76);
+        c.setQtdMg(1.63);
+        c.setQtdK(0.15);
+        c.setQtdHAl(5.35);
+        c.setTeorCO(0);
+        c.setPartDesejada(55);
+        assertEquals(55, c.calcPartCorrigidoCalcio(),0.1);
     }
 }
